8676c1c89871b615c4f87f51777042519b2eb4ae
Brandon Stair <Brandon Stair>
2016-03-29 19:12:18 -0700

    Initial commit

---
+++ app/src/main/java/com/example/broom_000/geo_quiz/Quiz_Activity.java
@@ -0,0 +1,14 @@
+package com.example.broom_000.geo_quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Quiz_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_);
+    }
+}
